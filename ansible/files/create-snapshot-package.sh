3e29ecee9ebc814c0714e343b178aa32b7b44fbf
Kouhei Sutou <kou@clear-code.com>
2014-11-17 10:58:05 +0900

    Fix indent

--- ansible/files/create-snapshot-package.sh
+++ ansible/files/create-snapshot-package.sh
@@ -132,10 +132,10 @@ package_mariadb_with_mroonga() {
 }
 
 create_nightly_build groonga groonga yes \
-    --without-cutter \
-    --enable-document \
-    --with-ruby \
-    --enable-mruby
+  --without-cutter \
+  --enable-document \
+  --with-ruby \
+  --enable-mruby
 create_nightly_build groonga groonga-normalizer-mysql yes
 build_mysql
 create_nightly_build mroonga mroonga no \
