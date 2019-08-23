1eb9aa67bf538b407727f37cd6fd9cae27a827e5
Kentaro Hayashi <hayashi@clear-code.com>
2019-08-23 18:33:37 +0900

    sentencepiece: remove unused files

--- sentencepiece/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(sentencepiece):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="sentencepiece" command="/usr/bin/sentencepiece"
--- sentencepiece/debian/shlibs.local.ex
+++
@@ -1,1 +0,0 @@
-libsentencepiece 0.1.83-Source sentencepiece (>> 0.1.83-Source-0), sentencepiece (<< 0.1.83-Source-99)
